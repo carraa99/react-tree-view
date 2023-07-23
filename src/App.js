b3b0cc90d8a3ef10384e21e0524f53a3eb0f9404
Chera <carawalteji@gmail.com>
2023-07-23 15:30:11 +0300

    delete by id is added

--- src/App.js
+++ src/App.js
@@ -91,37 +91,73 @@ const organizePositionsIntoTree = (positions) => {
 
 export default function App() {
   const [positions, setPositions] = useState([]);
-   const [positionTree, setPositionTree] = useState([]);
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/positions")
-      .then((response) => {
-        const organizedTree = organizePositionsIntoTree(response.data);
-        setPositions(organizedTree);
-        setPositionTree(response.data);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+  const [positionTree, setPositionTree] = useState([]);
+  const [positionsDeleted, setPositionsDeleted] = useState([]);
+  const [newPositions, setNewPositions] = useState([]);
 
-  // Modify renderSubtrees to use the fetched positions from the server
+
+    useEffect(() => {
+      axios
+        .get("http://localhost:5000/positions")
+        .then((response) => {
+          const organizedTree = organizePositionsIntoTree(response.data);
+          setPositions(organizedTree);
+          setPositionTree(response.data);
+        })
+        .catch((error) => console.error("Error fetching data:", error));
+    }, []);
+
   const renderSubtrees = (trees) => {
     return trees.map((tree) => {
       if (!tree.subtrees || tree.subtrees.length === 0) {
-        return <TreeItem key={tree.id} id={tree.id} title={tree.name} />;
+        return (
+          <TreeItem
+            key={tree.id}
+            id={tree.id}
+            title={tree.name}
+            onDeletePosition={handleDeletePosition}
+          />
+        );
       }
 
+      // If the tree has subtrees, recursively call renderSubtrees
+      // and remove the deleted position from the subtrees
+      const updatedSubtrees = tree.subtrees.filter(
+        (subtree) =>
+          !positionsDeleted.includes(subtree.id) &&
+          !newPositions.includes(subtree.id)
+      );
+
       return (
-        <TreeItem key={tree.id} id={tree.id} title={tree.name}>
-          {renderSubtrees(tree.subtrees)}
+        <TreeItem
+          key={tree.id}
+          id={tree.id}
+          title={tree.name}
+          onDeletePosition={handleDeletePosition}
+        >
+          {renderSubtrees(updatedSubtrees)}
         </TreeItem>
       );
     });
   };
+
+
+  const handleDeletePosition = (id) => {
+    // Update the state by adding the deleted position ID to positionsDeleted
+    setPositionsDeleted((prevPositionsDeleted) => [
+      ...prevPositionsDeleted,
+      id,
+    ]);
+  };
   const addNewPosition = (newPosition) => {
-    window.location.reload()
-    setPositions((prevPositions) => [...prevPositions, newPosition]);
-    setPositionTree((prevTree) => [...prevTree, newPosition]);
-};
+     window.location.reload();
+     setNewPositions((prevNewPositions) => [
+       ...prevNewPositions,
+       newPosition.id,
+     ]);
+     setPositions((prevPositions) => [...prevPositions, newPosition]);
+     setPositionTree((prevTree) => [...prevTree, newPosition]);
+   };
   return (
     <div className="App">
       <div className="mt-5 mb-16">
